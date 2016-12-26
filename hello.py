a4fc0c2e45c2549ac9b4fa39a0ead6881ad3a54c
tonysunyueran <yueran1@ualberta.ca>
2016-12-25 22:16:20 -0700

    add comments

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+#direct the page with the path end with '/' to hello world
 @app.route('/')
 def index():
     return '<h1>hello World!</h1>'
 
+#Direct the page with the path end with /user/<name> to hello user_name
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' %name
