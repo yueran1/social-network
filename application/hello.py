9fbcedd2264e3d420009705580d0540888791f33
tonysunyueran <yueran1@ualberta.ca>
2016-12-27 12:11:13 -0700

    add templates

--- application/hello.py
+++ application/hello.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 #direct the page with the path end with '/' to hello world
 @app.route('/')
 def index():
-    return '<h1>hello World!</h1>'
+    #return '<h1>hello World!</h1>'
+    return render_template('index.html')
 
 #Direct the page with the path end with /user/<name> to hello user_name
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' %name
+    #return '<h1>Hello, %s!</h1>' %name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
