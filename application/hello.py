40553af97c2490aeebcf4bf72e88244f61cf0d87
tonysunyueran <yueran1@ualberta.ca>
2016-12-31 23:30:40 -0700

    add database version index

--- application/hello.py
+++ application/hello.py
@@ -32,7 +32,7 @@ class Role(db.Model):
 	
 	#backref add a back reference in the other model in the relationship
 	#'User' represents the object-oriented view of the relationship
-	users= db.relationship('User', backref='role')
+	users= db.relationship('User', backref='role',lazy='dynamic')
 	
 	def __repr__(self):
 		return '<Role %r>' % self.name
@@ -67,18 +67,29 @@ def index():
     #When user navigate the app for 1st time, there is no name
     #After user submits a name, this name can be displayed on website
     if form.validate_on_submit():
-    	old_name= session.get('name')
+    	user = User.query.filter_by)(username=form.name.data).first()
+    	if user is None:
+    	
+			user= User(username= form.name.data)
+			db.session.add(user)
+			session['known']= False
+    	else:
+    		session['know']= True
+    	
+    	
+    	'''old_name= session.get('name')
     	if old_name is not None and old_name != form.name.data:
-    		flash("Gai Lai! name is different!")
+    		flash("Gai Lai! name is different!")'''
+    	
     	
     	#name= form.name.data
     	session['name']= form.name.data
     	
-    	#form.name.data= ''
+    	form.name.data= ''
     	#The only vaiable in url_for is enpoint, by default, endpoint's name is view 			#function's name
     	return redirect(url_for('index'))
     #Use GET instead of POST to avoid repeatly submit request
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 #Direct the page with the path end with /user/<name> to hello user_name
 @app.route('/user/<name>')
