b872bbdfd4c5a346f353f5cb6a633fb276e45d72
tonysunyueran <yueran1@ualberta.ca>
2017-01-29 07:21:58 -0700

    allow edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,19 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
 SubmitField
 from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
 from flask_pagedown.fields import PageDownField
 from ..models import Role, User
+from flask_login import login_user, login_required, logout_user, current_user
 
 #Required() validator ensures that the field is not submitted empty	
-class NameForm(FlaskForm):
+class NameForm(Form):
 	name=StringField('What is your name?', validators=[Required()])
 	submit= SubmitField('Submit')
+	
+class EditProfileForm(Form):
+	name = StringField('Real name', validators=[Length(0, 64)])
+	location = StringField('Location', validators=[Length(0,64)])
+	about_me = TextAreaField('About me')
+	submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, redirect, request, url_for, flash
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
+from flask_login import login_user, login_required, logout_user, current_user
 
 @main.route('/', methods=['GET','POST'])
 def index():
@@ -38,3 +40,19 @@ def user(username):
 		abort(404)
 	return render_template('user.html', user=user)
 
+@main.route('/edit-profile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.name= form.name.data
+		current_user.location= form.location.data
+		current_user.about_me = form.about_me.data
+		db.session.add(current_user)
+		flash('Your profile has been updated.')
+		return redirect(url_for('.user',username=current_user.username))
+	form.name.data=current_user.location
+	form.location.data=current_user.location
+	form.about_me.data= current_user.about_me
+	return render_template('edit_profile.html', form=form)
+	
