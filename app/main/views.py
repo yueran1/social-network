f13accc97c064caf34a4aebc37cc194733d7189c
tonysunyueran <yueran1@ualberta.ca>
2017-03-04 09:23:49 -0700

    allow edit post

--- app/main/views.py
+++ app/main/views.py
@@ -38,6 +38,26 @@ def user(username):
 	posts = user.posts.order_by(Post.timestamp.desc()).all()
 	return render_template('user.html', user=user,posts=posts)
 
+@main.route('/edit/<int:id>', methods=['GET','POST'])
+@login_required
+def edit(id):
+	post = Post.query.get_or_404(id)
+	if current_user != post.author and \
+			not current_user.can(Permission.ADMINISTER):
+		abort(403)
+	form = PostForm()
+	if form.validate_on_submit():
+		post.body=form.body.data
+		db.session.add(post)
+		flash('The post has been updated.')
+		return redirect(url_for('.post',id=post.id))
+	form.body.data=post.body
+	return render_template('edit_post.html', form=form)
+
+
+
+
+
 @main.route('/edit-profile',methods=['GET','POST'])
 @login_required
 def edit_profile():
