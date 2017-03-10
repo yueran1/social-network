a8a00cd149380878c663e26cd7e6ad5230b1041e
tonysunyueran <yueran1@ualberta.ca>
2017-03-10 11:49:06 -0700

    allow follower posts and all posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, current_app
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, make_response
 from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm,PostForm
 from .. import db
@@ -22,13 +22,34 @@ def index():
 		db.session.add(post)
 		return redirect(url_for('.index'))
 	#posts = Post.query.order_by(Post.timestamp.desc()).all()
+	
+	show_followed= False
+	if current_user.is_authenticated:
+		show_followed = bool(request.cookies.get('show_followed', ''))
+	
+	if show_followed:
+		query=current_user.followed_posts
+	else:
+		query=Post.query
+	
 	page = request.args.get('page',1,type=int)
-	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
+	pagination = query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
 	posts = pagination.items
-	return render_template('index.html', form=form, posts=posts, pagination=pagination)
+	return render_template('index.html', form=form, posts=posts, show_followed=show_followed, pagination=pagination)
     
+@main.route('/all')
+@login_required
+def show_all():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed','',max_age=30*24*60*60)
+	return resp
 
-    
+@main.route('/followed')
+@login_required
+def show_followed():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+	return resp    
     
 @main.route('/user/<username>')
 def user(username):
--- app/models.py
+++ app/models.py
@@ -187,6 +187,10 @@ class User(UserMixin, db.Model):
 	def is_followed_by(self, user):
 		return self.followers.filter_by(follower_id = user.id).first() is not None
 		
+	@property
+	def followed_posts(self):
+		return Post.query.join(Follow,Follow.followed_id ==Post.author_id).filter(Follow.follower_id == self.id)
+		
 class AnonymousUser(AnonymousUserMixin):
 	def can(self, permissions):
 		return False
